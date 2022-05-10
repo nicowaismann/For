dbcb165ee07524588d2801b3931810a9e3dd8fd2
nicowaismann <46961446@est.ort.edu.ar>
2022-05-10 09:00:01 -0300

    Initial commit

---
+++ Assets/instantiafor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiafor : MonoBehaviour
+{
+    public GameObject prefab;
+    public int cantidadDeClones;
+    public int grados;
+    
+
+    public void CloneObject()
+    {
+
+        GameObject clon;
+
+        for (int i = 0; i < cantidadDeClones; i++)
+        {
+            clon = Instantiate(prefab);
+            clon.transform.Rotate(0, grados*i, 0);
+            clon.transform.Translate(i, i, 0);
+            Destroy(clon,3);
+        }
+        
+    }
+}
